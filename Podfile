e9436699b8776c5437dacb994c4d197b595035ea
Dan Isacson <dan@themobilelife.com>
2016-11-29 20:16:18 +0100

    tab bar template project with pods setup.

---
+++ Podfile
@@ -0,0 +1,14 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '9.0'
+use_frameworks!
+
+target 'moviesensei' do
+	pod 'Alamofire'
+	pod 'Moya', '8.0.0-beta.4'
+	pod 'Moya/RxSwift'
+	pod 'RxSwift', '~> 3.0'
+	pod 'RxCocoa', '~> 3.0'
+    pod 'SwiftyJSON'
+end
+
---
+++ moviesenseiTests/moviesenseiTests.swift
@@ -0,0 +1,37 @@
+//
+//  moviesenseiTests.swift
+//  moviesenseiTests
+//
+//  Created by Dan Isacson on 2016-11-29.
+//  Copyright © 2016 dna. All rights reserved.
+//
+
+import XCTest
+@testable import moviesensei
+
+class moviesenseiTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        print("Test")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
